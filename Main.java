c510045d7c1d242aeb73912f2da59beea02e547b
Jmalone20 <jasmine.malone@patelhs.org>
2021-01-09 02:19:20 +0000

    I added a int array

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
   System.out.println("/n Here are the assigned seats: ");
    
   String[] names = {"Joe", "Josh", "Reese", "Reid", "Jasmine", "Lucy", "Nour", "Ruth", "Sam", "Sarah", "Harry", "Mark", "Eva", "Abbey", "Jane"}; 
-  int [] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};  
+  int [] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};   
   
    for (int i = 0; i < names.length; i++){
     System.out.println(names[i] + " Sits in Seat # " + seatNumbers[i]);
