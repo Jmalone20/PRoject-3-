c92a6470624e3dbfbffd9dd2ac67e6bc551df699
Jmalone20 <jasmine.malone@patelhs.org>
2021-01-09 02:21:47 +0000

    added a print statement to print the names and a short sentence

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
   int [] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};   
   
    for (int i = 0; i < names.length; i++){
-    System.out.println(names[i] + " Sits in Seat # " + seatNumbers[i]); 
+    System.out.println(names[i] + " Sits in Seat # " + seatNumbers[i]);   
     }
     
    int count = 15; 
